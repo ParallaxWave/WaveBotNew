259483a0d199f0e2cebec865a40af09a8ad90e23
Crypticwave <abhinavdatta38@gmail.com>
2021-04-13 15:19:05 +0530

    FIxedInvite

--- commands/invite.js
+++ commands/invite.js
@@ -2,8 +2,8 @@ const { embed } = require('../templates/completeEmbed');
 
 module.exports = {
   
-  name: 'test',
-  description: 'Testing the bot',
+  name: 'invite',
+  description: 'Invite the bot',
   category: 'Misc',
   execute(message, args){
     message.channel.send({ embed: embed('Click the link below to invite the bot!', '**https://discord.com/oauth2/authorize?client_id=757481381172609115&scope=bot&permissions=338690110**', message.author) });
